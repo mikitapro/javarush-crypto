a3474c755f1ac0c3c5dfbf478691dfe078f0c678
mikitapro <mikitapro@yandex.ru>
2022-03-21 18:05:52 +0300

    Add line-by-line encoding and decoding

--- srs/java/com/javarush/cryptoanalyer/Main.java
+++ srs/java/com/javarush/cryptoanalyer/Main.java
@@ -1,6 +1,53 @@
 package com.javarush.cryptoanalyer;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
+    private static final List<Character> ALPHABET = Arrays.asList('а', 'б', 'в',
+            'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у',
+            'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', '.', ',', '«', '»',
+            ':', '!', '?', ' ');
+
+    public static String stringCode(String text, int number) { // построчная кодировка
+        if (number < 0)
+            return "Wrong key";
+        int key = number % ALPHABET.size(); // получение ключа
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            char symbol = text.charAt(i);
+            if (ALPHABET.contains(symbol)) {
+                int oldIndex = ALPHABET.indexOf(symbol); // поиск индекса в алфавите
+                int newIndex = oldIndex + key; // новый индекс
+                if (newIndex >= ALPHABET.size()) {
+                    newIndex -= ALPHABET.size(); // сделать круг по алфавиту
+                }
+                char newSymbol = ALPHABET.get(newIndex); // новый символ
+                sb.append(newSymbol);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static String stringDecode(String text, int number) { // построчная декодировка
+        if (number < 0)
+            return "Wrong key";
+        int key = number % ALPHABET.size(); // получение ключа
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            char symbol = text.charAt(i);
+            if (ALPHABET.contains(symbol)) {
+                int oldIndex = ALPHABET.indexOf(symbol); // поиск индекса в алфавите
+                int newIndex = oldIndex - key;
+                if (newIndex < 0) {
+                    newIndex += ALPHABET.size(); // сделать круг по алфавиту
+                }
+                char newSymbol = ALPHABET.get(newIndex);
+                sb.append(newSymbol);
+            }
+        }
+        return sb.toString();
+    }
     public static void main(String[] args) {
         System.out.println("CryptoAnalyzer");
     }
